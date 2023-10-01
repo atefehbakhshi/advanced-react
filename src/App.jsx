5d186b241d92385bb7fc3ca55181b521bd178b67
Atefeh <at.bakhshi1373@gmail.com>
2023-10-01 16:22:14 +0330

    feat:useLayoutEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 function App() {
-  const [changeDogs, setChangeDogs] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [popupTop, setPopupTop] = useState(0);
+  const buttonRef = useRef(null);
+
+  // useEffect(() => {
+  //   if (buttonRef.current === null || !isOpen) return setPopupTop(0);
+  //   const { bottom } = buttonRef.current.getBoundingClientRect();
+  //   setPopupTop(bottom + 25);
+  // }, [isOpen]);
+
+  useLayoutEffect(() => {
+    if (buttonRef.current === null || !isOpen) return setPopupTop(0);
+    const { bottom } = buttonRef.current.getBoundingClientRect();
+    setPopupTop(bottom + 25);
+  }, [isOpen]);
+
+  //delay for 100ms
+  const now = performance.now();
+  while (now > performance.now() - 100) {
+    // Do nothing
+  }
+
   return (
-    <div>
-      {changeDogs ? <span>Dogs</span> : <span>Cats</span>}
-      <br />
-      <input type="text" key={changeDogs ? "dog" : "cat"} />
-      <button onClick={() => setChangeDogs((d) => !d)}>Switch</button>
-    </div>
+    <>
+      <button ref={buttonRef} onClick={() => setIsOpen((o) => !o)}>
+        show
+      </button>
+      {isOpen && (
+        <div
+          style={{
+            position: "absolute",
+            top: `${popupTop}px`,
+            border: "1px solid black",
+          }}
+        >
+          Tooltip
+        </div>
+      )}
+    </>
   );
 }
 
