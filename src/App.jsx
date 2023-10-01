0aaf03bfebbfe23dd15033d290aaf4a3aaa94e98
Atefeh <at.bakhshi1373@gmail.com>
2023-10-01 17:51:25 +0330

    feat:use 'useCallback' hook instead of 'useRef' hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import EmailForm from "./components/EmailForm";
+import { useCallback } from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
 function App() {
+  const [showInput, setShowInput] = useState(false);
+  const actualInputRef = useRef();
+
+  const inputRef = useCallback((input) => {
+    actualInputRef.current = input;
+    if (input === null) return;
+    input.focus();
+  }, []);
+
   return (
     <>
-      <EmailForm />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, ratione!
-      </p>
-      <EmailForm />
+      <button onClick={() => setShowInput(true)}>Toggle</button>
+      {showInput && <input type="text" ref={inputRef} />}
     </>
   );
 }
