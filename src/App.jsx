dd763591e03e19750bd2e0e984f1a6a9d0e87af4
Atefeh <at.bakhshi1373@gmail.com>
2023-10-01 17:12:23 +0330

    feat:useId hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,13 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import EmailForm from "./components/EmailForm";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [popupTop, setPopupTop] = useState(0);
-  const buttonRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (buttonRef.current === null || !isOpen) return setPopupTop(0);
-  //   const { bottom } = buttonRef.current.getBoundingClientRect();
-  //   setPopupTop(bottom + 25);
-  // }, [isOpen]);
-
-  useLayoutEffect(() => {
-    if (buttonRef.current === null || !isOpen) return setPopupTop(0);
-    const { bottom } = buttonRef.current.getBoundingClientRect();
-    setPopupTop(bottom + 25);
-  }, [isOpen]);
-
-  //delay for 100ms
-  const now = performance.now();
-  while (now > performance.now() - 100) {
-    // Do nothing
-  }
-
   return (
     <>
-      <button ref={buttonRef} onClick={() => setIsOpen((o) => !o)}>
-        show
-      </button>
-      {isOpen && (
-        <div
-          style={{
-            position: "absolute",
-            top: `${popupTop}px`,
-            border: "1px solid black",
-          }}
-        >
-          Tooltip
-        </div>
-      )}
+      <EmailForm />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, ratione!
+      </p>
+      <EmailForm />
     </>
   );
 }
